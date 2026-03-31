1dfe735560c6818ed1f7b6f03ab96bc91355f8f4
GS_anonimato <gsgabrielsousa132@gmail.com>
2026-03-30 22:21:19 -0300

    3.1.1

--- Formula_Matemáteca/ex011.php
+++ Formula_Matemáteca/ex011.php
@@ -8,6 +8,16 @@
 </head>
 <body>
 
+<?php 
+/*
+? Espaço amostral
+todo (chamamos o espaço amostral, e indicamos por Ω um conjunto formado por todos os resulktados possíveis de um experimento aleatório.)
+
+
+*/
+
+?>
+
     
 </body>
 </html>
